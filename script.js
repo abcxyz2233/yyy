b41b62d37c13f22327eeff49a3410732de967adb
abcxyz2233 <164356359+abcxyz2233@users.noreply.github.com>
2024-07-27 23:59:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const copyButtons = document.querySelectorAll('.copy-button');
 
 copyButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // Get the individual button's value directly from the dataset
     const value = button.dataset.value;
 
-    // Use `navigator.clipboard.writeText` to copy the content
     const successful = navigator.clipboard.writeText(value);
 
     if (successful) {
